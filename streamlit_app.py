ac855a111a28752753d8d4fd8c8b20adf25dfe55
moturichanakyareddy <123569856+moturichanakyareddy@users.noreply.github.com>
2023-02-07 16:14:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Morning Breakfast')
+
+streamlit.text('Dosa')
+streamlit.text('Idly & Sambar')
+streamlit.text('Bread and Butter')
+streamlit.text('otts & Milk')
